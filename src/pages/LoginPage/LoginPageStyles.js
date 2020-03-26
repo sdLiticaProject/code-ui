c3afe085b5d44941cb228f16ea8b9d7ca9b16fac
Anastasia <ejller@bk.ru>
2020-03-26 16:19:17 +0300

    Basic design of authorization

--- src/pages/LoginPage/LoginPageStyles.js
+++ src/pages/LoginPage/LoginPageStyles.js
@@ -1,6 +1,60 @@
-import styled from "styled-components";
+import styled, {keyframes} from "styled-components";
 
-const LoginWrapper = styled.div``;
+
+const LoginWrapper = styled.div`
+  box-sizing: border-box;
+  padding-top:10%;
+  width:  100%;
+  height: 100vh;
+  background-color: #E3ECF4;
+`;
+
+const move = keyframes`
+  from {
+    left: -80%;
+  }
+  to {
+    left: 100%;
+  }
+`;
+
+const CloudWrapper = styled.div`
+  z-index:0;
+  width:100%;
+  height: 300px;
+  overflow:hidden;
+  position: absolute;
+  bottom:0;
+`;
+
+const BackImage = styled.img `
+  height: 300px;
+  position: absolute;
+  left: -80%;
+  animation: ${move} ${props => props.timeAnimation} linear infinite;
+`;
+
+const Logo = styled.img `
+  width:230px;
+  position: relative;
+  top:-65px;
+`;
+
+
+
+
+
+const LoginFormWrapper = styled.div`
+  position: relative;
+  z-index: 1;
+  box-shadow: 0 10px 7px -10px rgba(0, 0, 0, 0.4);
+  text-align:center;
+  margin: auto;
+  background: white;
+  width:500px;
+  height: 550px;
+  border-radius: 10px;
+`;
 
 const Input = styled.input``;
 
@@ -8,4 +62,4 @@ const Button = styled.button`
   background: gray;
 `;
 
-export { LoginWrapper, Input, Button };
+export { LoginWrapper, LoginFormWrapper, Input, Button, BackImage, Logo, CloudWrapper };
--- src/pages/LoginPage/index.js
+++ src/pages/LoginPage/index.js
@@ -3,7 +3,7 @@ import {useState} from 'react';
 
 import {loginUser} from '../../actions/loginActions'
 import {useActions} from '../../hooks/useAction'
-
+import { LoginWrapper, Input, Button, LoginFormWrapper, BackImage, Logo, CloudWrapper } from "./LoginPageStyles";
 
 function AuthenticationPage() {
 
@@ -20,15 +20,26 @@ function AuthenticationPage() {
   }
 
   return(
-    <div>
-      <label>Login: </label>
-      <input placeholder="login" onChange={e => setLogin(e.target.value)}/>
-      <br/>
-      <label>Password: </label>
-      <input placeholder="password" onChange={e => setPassword(e.target.value)}/>
-      <br/>
-      <button onClick={submit}>Submit</button>
-    </div>
+    <LoginWrapper>
+        <CloudWrapper>
+          <BackImage src={process.env.PUBLIC_URL + '/image/cloud-2.png'} timeAnimation = "100s" />
+          <BackImage src={process.env.PUBLIC_URL + '/image/cloud.png'} timeAnimation = "70s"/>
+        </CloudWrapper>
+        <LoginFormWrapper>
+        <Logo src={process.env.PUBLIC_URL + '/image/logo.png'}/>
+        <br/>
+        <Input
+          placeholder="Write login"
+          onChange={e => setLogin(e.target.value)}
+        />
+        <br/>
+        <Input
+          placeholder="Write password"
+          onChange={e => setPassword(e.target.value)}
+        />
+        <Button onClick={submit}>check</Button>
+        </LoginFormWrapper>
+      </LoginWrapper>
   )
 }
 
