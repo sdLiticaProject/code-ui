7ab8da49adc78c85363c3b56809ea5e9c2c53ca4
Amadeus <seductoramadeus@gmail.com>
2021-06-02 11:33:13 +0300

    test master build

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,6 +3,7 @@ import loginReducer from './loginReducer';
 import userReducer from './userReducer';
 import controlReducer from './controlReducer';
 
+
 export const appReducer = combineReducers({
   login: loginReducer,
   user: userReducer,
